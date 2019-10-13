03485600364cdd26c774c6d6364f0f12014b9dd7
Suhas Bacchu <suhas.bacchu29@gmail.com>
2019-10-12 22:59:09 -0500

    Part 2 part2_chainproc.c started

---
+++ Project1/part2_chainproc.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void createChild(int i)
+{
+    if (fork() == 0)
+    {
+        printf("Hello from [child] pid %d from [parent] pid %d\n", getpid(), getppid());
+        if (i > 0)
+        {
+            createChild(i - 1);
+        }
+        else
+        {
+            exit(0);
+        }
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int i;
+    int pro = atoi(argv[1]);
+    createChild(pro);
+    // for ( i = 0; i < pro; i++){
+    //     if (fork() == 0){
+    //         printf("Hello from [child] pid %d from [parent] pid %d\n", getpid(), getppid());
+    //         exit(0);
+    //     }
+    // }
+    for (i = 0; i < pro; i++)
+        wait(NULL);
+}
