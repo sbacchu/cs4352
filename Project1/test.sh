142b4d2060f55c194b86f13780a45e305eb0ab4f
Suhas Bacchu <suhas.bacchu29@gmail.com>
2019-10-13 00:41:52 -0500

    Created test shell script

---
+++ Project1/test.sh
@@ -0,0 +1,28 @@
+make chainproc
+make multichild
+echo "Compilation Completed"
+echo "."
+echo "."
+ls
+echo "."
+echo "."
+echo "Testing Multiple Children Processes"
+./part2_multichild 2
+echo "."
+echo "."
+./part2_multichild 4
+echo "."
+echo "."
+./part2_multichild 8
+echo "."
+echo "."
+echo "."
+echo "."
+echo "Testing Chain Processes"
+./part2_chainproc 2
+echo "."
+echo "."
+./part2_chainproc 4
+echo "."
+echo "."
+./part2_chainproc 8
